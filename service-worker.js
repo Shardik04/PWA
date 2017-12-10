dd56db5fb91c919c89cad05d08e7eccafd767e42
Hardik Shah <hardik.4546@gmail.com>
2017-12-10 17:35:52 +0530

    condition for cache

--- service-worker.js
+++ service-worker.js
@@ -3,23 +3,27 @@ var cacheName = 'staticPWA-v1';
 self.addEventListener('install', function(event) {
   console.log('[ServiceWorker] Install');
   event.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.add([
-        '/',
-        '/index.html',
-        '/assets/css/main.css',
-        '/images/thumbs/01.jpg',
-        '/images/thumbs/02.jpg',
-        '/images/thumbs/03.jpg',
-        '/images/thumbs/04.jpg',
-        '/images/thumbs/05.jpg',
-        '/images/thumbs/06.jpg',
-        '/images/thumbs/07.jpg',
-        '/images/thumbs/08.jpg'
-      ]);
-      console.log("DONE");
-    })
+    if ('caches' in window) {
+      caches.open(cacheName).then(function(cache) {
+        console.log('[ServiceWorker] Caching app shell');
+        return cache.add([
+          '/',
+          '/index.html',
+          '/assets/css/main.css',
+          '/images/thumbs/01.jpg',
+          '/images/thumbs/02.jpg',
+          '/images/thumbs/03.jpg',
+          '/images/thumbs/04.jpg',
+          '/images/thumbs/05.jpg',
+          '/images/thumbs/06.jpg',
+          '/images/thumbs/07.jpg',
+          '/images/thumbs/08.jpg'
+        ]);
+        console.log("DONE");
+      })
+    } else {
+      console.log("cache not available");
+    }
   );
 });
 
