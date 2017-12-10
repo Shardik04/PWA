2edea8608d95566d48407fe66464d5d8f003c11d
Hardik Shah <hardik.4546@gmail.com>
2017-12-10 17:37:54 +0530

    file to cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,17 @@
 var cacheName = 'staticPWA-v1';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/assets/css/main.css',
+  '/images/thumbs/01.jpg',
+  '/images/thumbs/02.jpg',
+  '/images/thumbs/03.jpg',
+  '/images/thumbs/04.jpg',
+  '/images/thumbs/05.jpg',
+  '/images/thumbs/06.jpg',
+  '/images/thumbs/07.jpg',
+  '/images/thumbs/08.jpg'
+];
 
 self.addEventListener('install', function(event) {
   console.log('[ServiceWorker] Install');
@@ -6,19 +19,7 @@ self.addEventListener('install', function(event) {
     if ('caches' in window) {
       caches.open(cacheName).then(function(cache) {
         console.log('[ServiceWorker] Caching app shell');
-        return cache.add([
-          '/',
-          '/index.html',
-          '/assets/css/main.css',
-          '/images/thumbs/01.jpg',
-          '/images/thumbs/02.jpg',
-          '/images/thumbs/03.jpg',
-          '/images/thumbs/04.jpg',
-          '/images/thumbs/05.jpg',
-          '/images/thumbs/06.jpg',
-          '/images/thumbs/07.jpg',
-          '/images/thumbs/08.jpg'
-        ]);
+        return cache.addAll(filesToCache);
         console.log("DONE");
       })
     } else {
