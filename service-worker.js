61a923afa6fc0227ac186369c597f471db90a01a
Hardik Shah <hardik.4546@gmail.com>
2017-12-10 15:55:28 +0530

    service worker changes

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,20 @@
 var cacheName = 'staticPWA';
-// var filesToCache = [
-//   '/',
-//   '/index.html',
-//   '/assets/css',
-//   '/assets/fonts',
-//   '/assets/sass',
-//   '/assets/images'
-// ];
-
-function cacheAssets() {
-  return caches.open(cacheName)
-    .then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/assets/css/main.css',
-        '/assets/fonts',
-        '/assets/images'
-      ]);
-    });
-}
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/assets/css',
+  '/assets/fonts',
+  '/assets/sass',
+  '/assets/images'
+];
 
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Install');
   e.waitUntil(
-    cacheAssets()
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
   );
 });
 
