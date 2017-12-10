2dcf062a7214fa6ceff3c56ad689ec4d9f9a7551
Hardik Shah <hardik.4546@gmail.com>
2017-12-10 16:39:08 +0530

    changes

--- service-worker.js
+++ service-worker.js
@@ -28,8 +28,8 @@ self.addEventListener('install', function(e) {
 self.addEventListener('fetch', function(e) {
   console.log('[ServiceWorker] Fetch', e.request.url);
   e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+    caches.match(e.request).then(function() {
+      return fetch(e.request);
     })
   );
 });
