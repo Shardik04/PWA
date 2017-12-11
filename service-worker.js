01da1321babd7ce5d272cdf6159feb692f24be7d
Hardik Shah <hardik.4546@gmail.com>
2017-12-11 13:22:29 +0530

    update

--- service-worker.js
+++ service-worker.js
@@ -15,11 +15,13 @@ var filesToCache = [
 
 self.addEventListener('install', function(event) {
   console.log('[ServiceWorker] Install');
+  event.waitUntil(
     caches.open(cacheName).then(function(cache) {
       console.log('[ServiceWorker] Caching app shell');
-      return cache.add(filesToCache);
+      return cache.addAll(filesToCache);
       console.log("DONE");
     })
+  );
 });
 
 self.addEventListener('activate', function(e) {
@@ -28,7 +30,9 @@ self.addEventListener('activate', function(e) {
 
 self.addEventListener('fetch', function(e) {
   console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
     caches.match(e.request).then(function(response) {
       return response || fetch(e.request);
     })
+  );
 });
